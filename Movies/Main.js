43f35b9c3c720d72c5548b8cdefb432bc22f83b1
MoviesCluvb <80956909+MoviesCluvb@users.noreply.github.com>
2021-03-19 14:08:20 +0300

    Add files via upload

---
+++ Movies/Main.js
@@ -0,0 +1,16 @@
+function MainFunction()
+    {
+        var usrInpt = document.getElementById("usrInpt").value;
+        if(usrInpt == 1){
+           window.location.href = "NewAmesterdam.html"
+        }
+
+
+
+
+        else{
+            alert("عفوا! الرقم مو موجود");
+        }
+            
+        
+    }
